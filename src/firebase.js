4d7806fb1570a732e6f17c68108cd1cd9308ccfd
Shridhar Sarda <162474458+ShridharSarda@users.noreply.github.com>
2026-06-22 22:34:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA7prIEcxAdr_aGKHjilNYzurxphYf8JY0",
-  authDomain: "product-system-3f2d2.firebaseapp.com",
-  projectId: "product-system-3f2d2",
-  storageBucket: "product-system-3f2d2.firebasestorage.app",
-  messagingSenderId: "834879582990",
-  appId: "1:834879582990:web:d5a2b875f344fb50269d5f",
+const  
 };
 
 const app = initializeApp(firebaseConfig);
